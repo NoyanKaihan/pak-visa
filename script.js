db99a96f6174a31778548590d4abf069db532718
Kaihan <120945229+NoyanKaihan@users.noreply.github.com>
2025-01-11 01:50:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
 document.getElementById("menu-toggle").addEventListener("click", function () {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
