c682565ef0b6441702a1a1352c1efee2b315af5e
Kaihan <120945229+NoyanKaihan@users.noreply.github.com>
2025-01-10 19:15:41 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
-        alert("Navigation is not functional in this example.");
-    });
-});
