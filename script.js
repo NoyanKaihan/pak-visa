1bfe3b2b5c244cd272a1027fe992b56b507e9a2a
Kaihan <120945229+NoyanKaihan@users.noreply.github.com>
2025-01-10 17:16:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        alert("Navigation is not functional in this example.");
+    });
+});
